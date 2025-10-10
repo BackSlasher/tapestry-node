f156affa5133708c0f8446c91c2d50e613ea4b80
Nitzan Raz <nitz.raz@gmail.com>
2025-10-10 13:51:22 +0300

    sdkconfig.defaults to sdkconfig
    No need to keep defaults

--- build-ota.sh
+++ build-ota.sh
@@ -3,9 +3,4 @@ ESP_VERSION='v5.3.2'
 source esp-idf-helper $ESP_VERSION || exit 1
 
 set -euo pipefail
-
-# rebuild config file
-if [ -f sdkconfig ]; then
-    rm sdkconfig
-fi
 idf.py app
--- setup.sh
+++ setup.sh
@@ -48,11 +48,6 @@ wifi_password,data,string,$WIFI_PASSWORD
 screen_model,data,string,$SCREEN_MODEL
 EOF
 
-# rebuild config file
-if [ -f sdkconfig ]; then
-    rm sdkconfig
-fi
-
 # Build the project (this will now generate NVS partition automatically)
 echo "Building project with NVS generation..."
 idf.py build
